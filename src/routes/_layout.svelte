1dde99e7cc127e70c8021eb4b555d81b22835505
elianiva <dicha.arkana03@gmail.com>
2020-10-05 23:08:24 +0700

    chore: initial setup

---
+++ src/routes/_layout.svelte
@@ -0,0 +1,22 @@
+<style>
+main {
+  position: relative;
+  max-width: 56em;
+  background-color: white;
+  padding: 2em;
+  margin: 0 auto;
+  box-sizing: border-box;
+}
+</style>
+
+<Nav segment={segment} />
+
+<main>
+  <slot />
+</main>
+
+<script>
+import Nav from "../components/Nav.svelte"
+
+export let segment
+</script>
