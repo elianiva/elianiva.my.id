867d4f7a2b6133e1d7400358886106f1507dbec5
elianiva <dicha.arkana03@gmail.com>
2020-10-08 21:37:22 +0700

    feat: removed unused stylings

--- src/routes/_layout.svelte
+++ src/routes/_layout.svelte
@@ -1,22 +1,8 @@
-<style>
-main {
-  position: relative;
-  max-width: 56em;
-  background-color: white;
-  padding: 2em;
-  margin: 0 auto;
-  box-sizing: border-box;
-}
-</style>
+<Navbar segment={segment} />
 
-<Nav segment={segment} />
-
-<main>
-  <slot />
-</main>
+<slot />
 
 <script>
-import Nav from "../components/Nav.svelte"
-
+import Navbar from "../components/Navbar.svelte"
 export let segment
 </script>
