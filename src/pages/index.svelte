be3603729df6aaf2bf42022d4cda614e2d0755c3
elianiva <dicha.arkana03@gmail.com>
2020-10-15 15:13:35 +0700

    refactor: removed useless stuff

--- src/pages/index.svelte
+++ src/pages/index.svelte
@@ -34,37 +34,5 @@ import Section from "../parts/Sections.svelte"
 import OpenSauce from "../parts/OpenSauce.svelte"
 
 const posts = __POSTS__
-
 const projects = __PROJECTS__
-
-/* const projects = [ */
-/*   { */
-/*     title: "Kanaizu", */
-/*     desc: "A japanese kana quiz app built using Routify and TailwindCSS", */
-/*     cover: "projects/kanaizu.png", */
-/*     url: "/projects/kanaizu", */
-/*     date: "2020-10-01", */
-/*     demo: "https://kanaizu.vercel.app", */
-/*     source: "https://github.com/elianiva/kanaizu", */
-/*   }, */
-/*   { */
-/*     title: "Skaga", */
-/*     desc: "SMKN 3 Jember's site remake using NextJS and TailwindCSS", */
-/*     cover: "projects/skaga.png", */
-/*     url: "/projects/skaga", */
-/*     date: "2020-10-10", */
-/*     demo: "https://skaga.vercel.app", */
-/*     source: "https://github.com/elianiva/skaga", */
-/*   }, */
-/*   { */
-/*     title: "Three of Something", */
-/*     desc: */
-/*       "A website to put all of the result of a challenge that I did every week", */
-/*     cover: "projects/tos.png", */
-/*     url: "/projects/tos", */
-/*     date: "2020-10-10", */
-/*     demo: "https://three-of-something.vercel.app", */
-/*     source: "https://github.com/elianiva/three-of-something", */
-/*   }, */
-/* ] */
 </script>
