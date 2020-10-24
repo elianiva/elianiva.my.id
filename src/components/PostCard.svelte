f574bed30dfee5bdaf66b61696aca4a5e85b87d8
elianiva <dicha.arkana03@gmail.com>
2020-10-25 01:16:59 +0700

    feat: new postcard

--- src/components/PostCard.svelte
+++ src/components/PostCard.svelte
@@ -5,48 +5,43 @@
   border-radius: 0.25rem;
   text-align: left;
   background-color: #ffffff;
-}
-
-.card__img {
-  position: relative;
-  display: block;
-  width: 100%;
   z-index: 2;
 }
 
 .card__details {
-  color: #121212;
+  color: #292021;
   text-decoration: none;
   padding: 1rem;
   display: grid;
-  grid-template-rows: 2rem 3.5rem 5rem 1fr;
-  border-top: 0.0625rem #e4e4e4 solid;
+  grid-template-rows: 3.5rem 2rem 5.5rem 1fr;
   transition: all ease-out 0.2s;
 }
 
-.card__details span {
-  line-height: 1.5rem;
-}
-
 .card__title {
-  font-family: "Roboto Condensed", sans-serif;
+  font-family: "Overpass", sans-serif;
   font-size: 1.25rem;
   font-weight: 600;
   line-height: 1.75rem;
 }
 
 .card__desc {
+  border-top: 0.0625rem #e4e4e4 solid;
+  font-family: "Open Sans", sans-serif;
   line-height: 1.5rem;
   color: #696969;
-  font-family: "Roboto", sans-serif;
+  margin-top: 0.25rem;
+  padding-top: 0.5rem;
 }
 
 .card__date {
-  font-family: "Roboto Condensed", sans-serif;
+  font-family: "Overpass", sans-serif;
   display: flex;
   align-items: center;
+  justify-self: start;
+  font-size: 0.8rem;
   gap: 0.25rem;
   color: #696969;
+  border-radius: 0.25rem;
 }
 
 .card__tags {
@@ -55,13 +50,15 @@
 }
 
 .card__tag {
-  background-color: #ff4851;
-  color: #ffffff;
-  font-family: "Roboto", sans-serif;
+  /* background-color: #ff4851; */
+  font-family: "Overpass", sans-serif;
+  font-weight: 600;
+  color: #292021;
+  font-size: 0.8rem;
   text-decoration: none;
   gap: 0.5rem;
-  padding: 0.25rem 0.5rem;
-  border-radius: 0.25rem;
+  padding: 0.5rem 0 0;
+  border-radius: 1rem;
   transition: filter ease-out 0.2s;
   text-transform: capitalize;
 }
@@ -76,8 +73,8 @@
 }
 
 :global(.card__icon) {
-  width: 1.125rem;
-  height: 1.125rem;
+  width: 0.5rem;
+  height: 0.5rem;
 }
 
 @media only screen and (min-width: 480px) {
@@ -88,32 +85,21 @@
 </style>
 
 <div class="card">
-  <Waypoint throttle="500" offset="320">
-    <img class="card__img" src={src} alt={title} />
-  </Waypoint>
   <a rel="prefetch" href={href} class="card__details">
+    <span class="card__title">{title}</span>
     <div class="card__date">
       <Calendar class="card__icon" />
       <span>{dayjs(date).format('DD MMMM YYYY')}</span>
     </div>
-    <span class="card__title">{title}</span>
     <p class="card__desc">{desc}</p>
     <div class="card__tags">
-      {#each tags as tag}
-        <a
-          class="card__tag"
-          target="_blank"
-          rel="norel noreferrer"
-          href="/"
-        >{tag}</a>
-      {/each}
+      {#each tags as tag}<span class="card__tag">{tag}</span>{/each}
     </div>
   </a>
 </div>
 
 <script>
 import Calendar from "../icons/calendar.svg"
-import Waypoint from "svelte-waypoint"
 import dayjs from "dayjs"
-export let title, src, desc, href, date, tags
+export let title, desc, href, date, tags
 </script>
