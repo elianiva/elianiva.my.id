da5b0e32facdde4bde3d1798010b0d8af57362be
elianiva <dicha.arkana03@gmail.com>
2020-11-16 14:53:01 +0700

    feat: error page styling

--- src/pages/_error.svelte
+++ src/pages/_error.svelte
@@ -1,33 +1,73 @@
 <style>
-h1,
-p {
-  margin: 0 auto;
+.err {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  color: var(--color-main-text);
+  text-align: center;
+  padding: 0 1rem;
 }
 
-h1 {
-  font-size: 2.8em;
-  font-weight: 700;
-  margin: 0 0 0.5em 0;
+.err__status {
+  font-size: 4rem;
+  font-family: "Overpass", sans-serif;
+  margin-top: 4rem;
 }
 
-p {
-  margin: 1em auto;
+.err__msg {
+  font-size: 2rem;
+  font-family: "Open Sans", sans-serif;
 }
 
-@media (min-width: 480px) {
-  h1 {
-    font-size: 4em;
-  }
+.err__url {
+  font-weight: 600;
+  position: relative;
+  display: inline-block;
+  color: var(--color-main-accent);
+  text-decoration: none;
+  transition: all ease-out 0.2s;
+}
+
+.err__url::before {
+  position: absolute;
+  content: "";
+  bottom: -0.25rem;
+  left: -0.25rem;
+  right: -0.25rem;
+  top: 0.25rem;
+  transform: scale3d(0, 0.1, 1);
+  transform-origin: 0 100%;
+  background-image: linear-gradient(
+    to right,
+    var(--color-main-accent),
+    rgba(0, 0, 0, 0)
+  );
+  z-index: -1;
+  transition: transform ease-out 0.2s;
+}
+
+.err__url:hover::before {
+  transform: scale3d(1, 0.1, 1);
 }
 </style>
 
 <svelte:head>
-  <title>{status}</title>
+  <title>{status} | Elianiva's Site</title>
 </svelte:head>
 
-<h1>{status}</h1>
-
-<p>{error.message}</p>
+<div class="err">
+  {#if status === 404}
+    <h1 class="err__status">{status}</h1>
+    <span class="err__msg">Sorry, you might entered a wrong URL.
+      <br />
+      Wanna go
+      <a class="err__url" href="/">back home?</a>
+    </span>
+  {:else}
+    <span class="err__msg"> Something went wrong. </span>
+  {/if}
+</div>
 
 {#if dev && error.stack}
   <pre>{error.stack}</pre>
