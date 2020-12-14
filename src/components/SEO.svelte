ec0c77db0989c486121b07096fe4fe41d33e0461
elianiva <dicha.arkana03@gmail.com>
2020-12-14 13:50:35 +0700

    fix: attempt to fix SEO

--- src/components/SEO.svelte
+++ src/components/SEO.svelte
@@ -7,11 +7,9 @@
   <!-- Open Graph / Facebook -->
   <meta property="og:type" content={isPost ? 'blog' : 'website'} />
   <meta property="og:url" content="{siteUrl}{$page.path}" />
-  <meta property="og:title" content={title | siteName} />
+  <meta property="og:title" content={title || siteName} />
   <meta property="og:description" content={desc} />
-  {#if thumbnail}
-    <meta property="og:image" content={thumbnail} />
-  {/if}
+  <meta property="og:image" content={thumbnail || 'https://avatars3.githubusercontent.com/u/51877647?s=240&v=4'} />
 
   <!-- Twitter -->
   <meta
@@ -19,7 +17,7 @@
     content={thumbnail ? 'summary_large_image' : 'summary'}
   />
   <meta property="twitter:url" content="{siteUrl}{$page.path}" />
-  <meta property="twitter:title" content={title | siteName} />
+  <meta property="twitter:title" content={title || siteName} />
   <meta property="twitter:description" content={desc} />
   {#if thumbnail}
     <meta property="twitter:image" content={thumbnail} />
@@ -34,7 +32,7 @@ const { page } = stores()
 
 const { siteName, siteUrl } = data
 
-export let title = ""
+export let title
 export let isPost = false
 export let thumbnail = false
 export let desc = data.desc
