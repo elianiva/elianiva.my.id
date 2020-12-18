853a19bc007c1499579276a64322e5bbb5eb6637
elianiva <dicha.arkana03@gmail.com>
2020-12-18 17:43:42 +0700

    fix: wrong img url

--- src/parts/Sections.svelte
+++ src/parts/Sections.svelte
@@ -86,7 +86,7 @@
       {#each data as item}
         <ProjectCard
           title={item.title}
-          imgSrc={`/assets/project/${item.slug}/cover.png`}
+          imgSrc={`/assets/project/${item.slug}/cover.webp`}
           href={`/project/${item.slug}`}
           desc={item.desc}
           demo={item.demo}
