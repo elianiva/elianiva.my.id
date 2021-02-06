2d8af29eef00e5619fbef70160db8f635e62a96f
elianiva <dicha.arkana03@gmail.com>
2021-02-06 12:32:09 +0700

    fix: resolve to absolute path

--- src/utils/fetch-all.js
+++ src/utils/fetch-all.js
@@ -1,18 +1,23 @@
 import fs from "fs"
 import frontmatter from "front-matter"
+import path from "path"
 
 const HAS_EXTENSION = /\.[^/.]+$/
+const getPagePath = kind => path.resolve(`./src/pages/${kind}`)
 
 export const getResources = kind => {
   if (!kind) throw new Error("KIND IS REQUIRED!")
 
   const result = fs
-    .readdirSync(`./src/pages/${kind}`)
+    .readdirSync(getPagePath(kind))
     .filter(file => !HAS_EXTENSION.test(file) && `${file}/index.svx`)
     .map(fileName => {
-      const postContent = fs.readFileSync(`./src/pages/${kind}/${fileName}/index.svx`, {
-        encoding: "utf8",
-      })
+      const postContent = fs.readFileSync(
+        `${getPagePath}/${fileName}/index.svx`,
+        {
+          encoding: "utf8",
+        }
+      )
       const { attributes } = frontmatter(postContent)
 
       return {
