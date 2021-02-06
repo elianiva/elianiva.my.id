1d2a91b728adf418527c890b3ee12ca504e86355
elianiva <dicha.arkana03@gmail.com>
2021-02-06 09:10:56 +0700

    feat: added API

---
+++ src/pages/api/post/[path].json.js
@@ -0,0 +1,23 @@
+// TODO(elianiva): fix this stupid path later
+import { getResources } from "../../../utils/fetch-all.js"
+
+export async function get(req, res, next) {
+  const { path } = req.params
+  const { limit } = req.query
+
+  let result = getResources("post")
+
+  if (path && path !== "all") {
+    result = result.find(item => item.slug === path)
+  }
+
+  if (limit) result = result.slice(0, limit)
+
+  if (result !== null) {
+    res.setHeader("Content-Type", "application/json")
+    res.end(JSON.stringify(result))
+    return
+  }
+
+  next()
+}
