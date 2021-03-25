3ac21d6a4af99ccefda4907edd89df9047936875
elianiva <dicha.arkana03@gmail.com>
2021-03-25 08:53:48 +0700

    feat: add SEO component

---
+++ src/lib/components/SEO.svelte
@@ -0,0 +1,48 @@
+<svelte:head>
+  <title>{title} | {siteName}</title>
+  <link rel="canonical" href="{siteUrl}{$page.path}" />
+  <meta name="description" content={desc} />
+  <meta name="keywords" content={keywords.join(",").toLowerCase()} />
+
+  <!-- Open Graph / Facebook -->
+  <meta property="og:type" content={isPost ? "blog" : "website"} />
+  <meta property="og:url" content="{siteUrl}{$page.path}" />
+  <meta property="og:title" content={title || siteName} />
+  <meta property="og:description" content={desc} />
+  <meta
+    property="og:image"
+    content={thumbnail
+      ? thumbnail.toString()
+      : "https://avatars3.githubusercontent.com/u/51877647?s=240&v=4"}
+  />
+
+  <!-- Twitter -->
+  <meta
+    property="twitter:card"
+    content={thumbnail ? "summary_large_image" : "summary"}
+  />
+  <meta property="twitter:url" content="{siteUrl}{$page.path}" />
+  <meta property="twitter:title" content={title || siteName} />
+  <meta property="twitter:description" content={desc} />
+  {#if thumbnail}
+    <meta
+      property="twitter:image"
+      content={thumbnail ? thumbnail.toString() : ""}
+    />
+  {/if}
+</svelte:head>
+
+<script lang="ts">
+import { stores } from "$app/stores"
+import data from "$lib/data/site"
+
+const { page } = stores()
+
+const { siteName, siteUrl } = data
+
+export let title: string
+export let isPost: boolean = false
+export let thumbnail: boolean = false
+export let desc: string = data.desc
+export let keywords: string[] = data.keywords
+</script>
