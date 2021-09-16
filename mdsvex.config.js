ee05c013484ea50340c74349d1548171240e3e8c
elianiva <dicha.arkana03@gmail.com>
2021-09-16 12:43:31 +0700

    chore: bump deps, remove useless deps

--- mdsvex.config.js
+++ mdsvex.config.js
@@ -1,20 +1,6 @@
 import remarkTOC from "remark-toc";
 import rehypeSlug from "rehype-slug";
 import rehypeAutolink from "rehype-autolink-headings";
-import * as shiki from "shiki";
-
-// fix whitespace weirdness
-const escape = code => {
-  return code
-    .replace(/[{}`]/g, c => ({ "{": "&#123;", "}": "&#125;", "`": "&#96;" }[c]))
-    .replace(/\\([trn])/g, "&#92;$1");
-};
-
-const highlighter = async (code, lang) => {
-  const highlighter = await shiki.getHighlighter({ theme: "github-dark" });
-  const highlightedCode = highlighter.codeToHtml(code, lang || "text");
-  return `{@html \`${escape(highlightedCode)}\` }`;
-};
 
 export default {
   layout: {
