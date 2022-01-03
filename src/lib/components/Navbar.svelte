034ed9d2c244110b4e2d5741c2416a428798b8d2
elianiva <dicha.arkana03@gmail.com>
2022-01-03 20:44:03 +0700

    refactor: blurs are overrated, man

--- src/lib/components/Navbar.svelte
+++ src/lib/components/Navbar.svelte
@@ -2,8 +2,7 @@
 .navbar {
   position: sticky;
   height: 4rem;
-  backdrop-filter: saturate(180%) blur(1.5rem);
-  background-color: rgba(var(--color-alt-bg-rgb), 0.7);
+  background-color: var(--color-alt-bg);
   z-index: 30;
   left: 0;
   right: 0;
