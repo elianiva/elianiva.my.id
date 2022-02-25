cffc2464da511c72bee36392278cdbb41f96e6fc
elianiva <dicha.arkana03@gmail.com>
2022-02-25 21:45:42 +0700

    refactor: use adapter-static

--- svelte.config.js
+++ svelte.config.js
@@ -1,19 +1,19 @@
-import { mdsvex } from "mdsvex"
-import mdsvexConfig from "./mdsvex.config.js"
-import adapterVercel from "@sveltejs/adapter-vercel"
-import sveltePreprocess from "svelte-preprocess"
-import Icons from 'unplugin-icons/vite'
-import Unocss from "unocss/vite"
+import { mdsvex } from "mdsvex";
+import mdsvexConfig from "./mdsvex.config.js";
+import adapterStatic from "@sveltejs/adapter-static";
+import sveltePreprocess from "svelte-preprocess";
+import Icons from "unplugin-icons/vite";
+import Unocss from "unocss/vite";
 
 /** @type {import('@sveltejs/kit').Config} */
 export default {
   extensions: [".svelte", ...mdsvexConfig.extensions],
   preprocess: [mdsvex(mdsvexConfig), sveltePreprocess()],
   kit: {
-    adapter: adapterVercel(),
+    adapter: adapterStatic(),
     inlineStyleThreshold: 1024,
     vite: {
-      plugins: [Icons({ compiler: "svelte" }), Unocss()]
+      plugins: [Icons({ compiler: "svelte" }), Unocss()],
     },
   },
-}
+};
