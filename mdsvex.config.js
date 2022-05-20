7bd6392f2a314056a1f88c0e7bdd7481ff84a213
elianiva <dicha.arkana03@gmail.com>
2022-05-20 22:44:30 +0700

    chore: remove useless layout

--- mdsvex.config.js
+++ mdsvex.config.js
@@ -8,8 +8,6 @@ const highlighter = await getHighlighter({ theme: "github-dark" });
 export default defineMDSveXConfig({
   layout: {
     _: "./src/lib/layouts/post.svelte",
-    post: "./src/lib/layouts/post.svelte",
-    project: "./src/lib/layouts/project.svelte",
     about: "./src/lib/layouts/about.svelte",
   },
   extensions: [".svx", ".md"],
