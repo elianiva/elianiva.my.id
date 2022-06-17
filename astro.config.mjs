7d7c453f8b2dda95bdfdc5b9da15e9a8c8ba5539
elianiva <dicha.arkana03@gmail.com>
2022-06-18 06:53:35 +0700

    chore: setup editor and less

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,8 @@
-import { defineConfig } from 'astro/config';
+import { defineConfig } from "astro/config";
 
 import solid from "@astrojs/solid-js";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [solid()]
+	integrations: [solid()],
 });
