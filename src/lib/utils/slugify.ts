7a4f0f3dc5a04fa5c267a231ba563c551c680ad0
elianiva <dicha.arkana03@gmail.com>
2022-10-08 16:42:20 +0700

    chore: clean slate

--- src/lib/utils/slugify.ts
+++
@@ -1,6 +0,0 @@
-export function slugify(content: string) {
-	return content
-		.toLowerCase()
-		.replace(/\s+/g, "-")
-		.replace(/[^\w-]+/g, "");
-}
--- vite.config.js
+++
@@ -1,8 +0,0 @@
-import { defineConfig } from "vite";
-import Icons from "unplugin-icons/vite";
-import Unocss from "unocss/vite";
-import { sveltekit } from "@sveltejs/kit/vite";
-
-export default defineConfig({
-	plugins: [sveltekit(), Icons({ compiler: "svelte" }), Unocss()],
-});
