2b68200ae73f381ffaeebbca53acc7a52c8ecd96
sglkc <31957516+sglkc@users.noreply.github.com>
2022-10-14 23:38:42 +0700

    feat(tag): add tabindex

--- src/components/Tag.svelte
+++ src/components/Tag.svelte
@@ -1,5 +1,6 @@
 <span
 	class={`font-heading font-medium ${variant === "ghost" ? "bg-rose-100 text-rose-700" : "shadow-sharp border-2 border-rose-900 bg-white text-zinc-900"} text-sm no-underline gap-2 py-1 px-2 mt-2 uppercase transition-property-filter duration-200 ease-out cursor-pointer`}
+	tabindex="0"
 	on:click={() => onClick?.()}
 >
 	<slot />
