5e0916953977e47bd54027af1ed747ec4c664d5f
elianiva <dicha.arkana03@gmail.com>
2023-04-14 08:48:07 +0700

    refactor: remove unused files

--- src/services/constants.ts
+++
@@ -1,8 +0,0 @@
-export const POSTS = import.meta.glob("/data/posts/**/index.svx", {
-	as: "raw",
-	eager: true,
-});
-export const PROJECTS = import.meta.glob("/data/projects/**/index.svx", {
-	as: "raw",
-	eager: true,
-});
--- src/services/types/heading.ts
+++
@@ -1,4 +0,0 @@
-export interface Heading {
-	level: number;
-	value: string;
-}
