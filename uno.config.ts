9b0a1ca41f0933e4924a233ccd6808aca5f853f5
elianiva <51877647+elianiva@users.noreply.github.com>
2025-03-17 20:31:22 +0700

    refactor: use biome instead, bump deps

--- uno.config.ts
+++ uno.config.ts
@@ -1,41 +1,43 @@
-import { defineConfig } from "unocss/vite";
-import { presetWind, presetTypography } from "unocss";
-import { colors } from "unocss/preset-wind"
+import { defineConfig, presetTypography, presetWind4 } from "unocss";
+import { theme } from "@unocss/preset-wind4";
 
 export default defineConfig({
-	presets: [presetWind(), presetTypography({
-		cssExtend: {
-			"h1": {
-				"font-size": "clamp(1.5rem, 5vw, 2rem)",
-				"line-height": "1.2em",
-				"text-align": "left"
+	presets: [
+		presetWind4(),
+		presetTypography({
+			cssExtend: {
+				h1: {
+					"font-size": "clamp(1.5rem, 5vw, 2rem)",
+					"line-height": "1.2em",
+					"text-align": "left",
+				},
+				h2: {
+					"font-size": "clamp(1.25rem, 5vw, 1.75rem)",
+					"line-height": "1.2em",
+					"text-align": "left",
+				},
+				h3: {
+					"font-size": "clamp(1.125rem, 5vw, 1.5rem)",
+					"line-height": "1.2em",
+					"text-align": "left",
+				},
+				h4: {
+					"font-size": "clamp(1rem, 5vw, 1.25rem)",
+					"line-height": "1.2em",
+					"text-align": "left",
+				},
+				pre: {
+					"border-radius": "0.25rem",
+					border: `1px solid ${theme.colors.pink[300]}`,
+					padding: "0.75rem",
+				},
 			},
-			"h2": {
-				"font-size": "clamp(1.25rem, 5vw, 1.75rem)",
-				"line-height": "1.2em",
-				"text-align": "left"
-			},
-			"h3": {
-				"font-size": "clamp(1.125rem, 5vw, 1.5rem)",
-				"line-height": "1.2em",
-				"text-align": "left"
-			},
-			"h4": {
-				"font-size": "clamp(1rem, 5vw, 1.25rem)",
-				"line-height": "1.2em",
-				"text-align": "left"
-			},
-			"pre": {
-				"border-radius": "0.25rem",
-				"border": `1px solid ${colors.pink[300]}`,
-				"padding": "0.75rem",
-			}
-		}
-	})],
+		}),
+	],
 	theme: {
-		fontFamily: {
+		font: {
 			serif: "'Lora', serif",
 			mono: "'IBM Plex Mono', monospace",
-		}
-	}
+		},
+	},
 });
