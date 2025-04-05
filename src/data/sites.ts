5131ef1252dd16d410b1b92bf850213002e630f4
elianiva <51877647+elianiva@users.noreply.github.com>
2025-04-05 23:56:14 +0700

    refactor: update seo a bit

--- src/data/sites.ts
+++ src/data/sites.ts
@@ -1,5 +1,6 @@
 export default {
-	siteName: "Elianiva",
+	author: "elianiva",
+	siteName: "elianiva's home row",
 	siteUrl: import.meta.env.DEV
 		? "http://localhost:4321"
 		: "https://elianiva.my.id",
@@ -9,6 +10,6 @@ export default {
 	linkedin: "https://www.linkedin.com/in/dichaa",
 	cv: "/assets/cv_dicha.pdf",
 	email: "contact@elianiva.my.id",
-	description: "Elianiva's personal website",
-	keywords: ["personal", "website", "blog", "article"],
+	description: "elianiva's home row",
+	keywords: ["personal", "website", "blog", "article", "portfolio", "elianiva"],
 } as const;
