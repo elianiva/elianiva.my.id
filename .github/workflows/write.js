23392e9636ddbe9ec12ce05bb0a58e4d72774d14
elianiva <51877647+elianiva@users.noreply.github.com>
2025-04-10 15:51:23 +0700

    fix: try moving require to function body

--- .github/workflows/write.js
+++ .github/workflows/write.js
@@ -1,7 +1,7 @@
-const fs = require("node:fs");
-const path = require("node:path");
-
 module.exports = async ({ github, context, core }) => {
+	const fs = require("node:fs");
+	const path = require("node:path");
+
 	const issue = await github.rest.issues.get({
 		issue_number: context.issue.number,
 		owner: context.repo.owner,
